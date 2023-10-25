011ea2b3047ada8ec2a3c5c12326997c5f37a837
shawn <shawn.dagisi@student.moringaschool.com>
2023-10-25 19:26:57 +0300

    rendering breed list

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 const breedInput = document.querySelector("#breedInput");
 const breedName = document.querySelector("#breedName");
 const dogImage = document.querySelector("#dogImage");
+const breedDetails = document.querySelector("#breedDetails");
+const breedLikes = document.querySelector("#likes");
 
 const LIVEENV = true;
 let currentBreed = {};
@@ -25,5 +27,39 @@ function fetchRandomImage(breed = null) {
 		: "https://dog.ceo/api/breeds/image/random";
 	return axios.get(url).catch((err) => {
 		console.log(err);
+	});
+}
+
+function fetchBreedList() {
+	url = LIVEENV
+		? "https://dog.ceo/api/breeds/list/all"
+		: "http://localhost:3000/breeds";
+	return axios
+		.get(url, {
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+}
+
+function renderBreedList(res) {
+	if (LIVEENV) {
+		const breeds = res.data.message;
+		const breedList = Object.keys(breeds);
+		breedList.forEach((breed) => {
+			const breedOption = document.createElement("option");
+			breedOption.textContent = breed;
+			breedInput.appendChild(breedOption);
+		});
+		return;
+	}
+	res.data.forEach((breed) => {
+		option = document.createElement("option");
+		option.value = breed.name;
+		option.textContent = breed.name;
+		breedInput.appendChild(option);
 	});
 }
